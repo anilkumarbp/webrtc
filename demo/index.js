754af4bbfb77d4c57fdfb7520a382041ba12bde1
vyshakhbabji <vyshakh.babji@gmail.com>
2016-01-28 22:54:44 -0800

    Updated Demo for Console Logs.

--- demo/index.js
+++ demo/index.js
@@ -3,19 +3,13 @@ var webPhone = RingCentral.WebPhone;
 
 localStorage.webPhoneUUID = localStorage.webPhoneUUID || webPhone.utils.uuid();
 
-
-
 var platform ;
 
 function startCall(toNumber, fromNumber) {
     if (fromNumber == "")
         alert('Fill in the number');
     else {
-
-
         fromNumber = fromNumber || localStorage.webPhoneLogin;
-
-        console.log('SIP call to', toNumber, 'from', fromNumber);
         var countryId = null;
         platform
             .get('/restapi/v1.0/account/~/extension/~')
@@ -26,69 +20,78 @@ function startCall(toNumber, fromNumber) {
                 }
             })
             .then(function () {
+                console.log('SIP call to', toNumber, 'from', fromNumber);
                 webPhone.sipUA.call(toNumber, fromNumber, countryId);
             });
     }
 }
 
-
-
 function mute(){
     webPhone.sipUA.mute(line);
+    console.log('Call Mute');
 }
 
 function unmute(){
     webPhone.sipUA.unmute(line);
+    console.log('Call Unmute');
 }
 
 function hold(){
     webPhone.sipUA.hold(line);
+    console.log('Call Hold');
 }
 
 function unhold(){
     webPhone.sipUA.unhold(line);
+    console.log('Call UnHold');
 }
 
 function answerIncomingCall(){
     webPhone.sipUA.answer(line);
+    console.log('Answering Incoming Call');
 }
 
 function disconnect(){
-
-    line.hangup();
+    webPhone.sipUA.hangup(line);
+    console.log('Hangup Call');
 }
 
 function isOnCall(){
-
     return webPhone.sipUA.onCall();
 }
 
 
 function reregister(){
     webPhone.sipUA.reregister();
+    console.log('Reregistered SIP');
 }
 
 
 function unregisterSip(){
     webPhone.sipUA.unregister();
+    console.log('Unregistered SIP');
 }
 
 function forceDisconnectSip(){
     webPhone.sipUA.forceDisconnect();
+    console.log('Forcing SIP disconnection');
 }
 
 
 function startRecording(){
     line.record(true);
+    console.log('Start Recording Call');
 }
 
 function stopRecording(){
-        line.record(false);
+    line.record(false);
+    console.log('Stop Recording Call');
 }
 
 
 function callpark(){
-        line.park();
+    line.park();
+    console.log('Call Parking');
 }
 
 function callflip(number){
@@ -101,22 +104,28 @@ function callflip(number){
 function callTransfer(number){
     if(number=="")
         alert('Fill in the number');
-    else
-    line.transfer(number)
+    else {
+        line.transfer(number)
+        console.log('Call Transfer');
+    }
 }
 
 function sendDTMF(DTMF){
     if(DTMF=="")
         alert('Fill in the DTMF');
-    else
-    line.sendDTMF(DTMF)
+    else {
+        line.sendDTMF(DTMF)
+        console.log('Send DTMF'+DTMF);
+    }
 }
 
-function forward(number){
-    if(number=="")
+function forward(number) {
+    if (number == "")
         alert('Fill in the number');
-    else
-    line.forward(number)
+    else {
+        line.forward(number)
+        console.log('Call Forwarding');
+    }
 }
 
 function registerSIP(checkFlags, transport) {
@@ -146,7 +155,6 @@ function registerSIP(checkFlags, transport) {
             if (endpointId) {
                 headers.push('P-rc-endpoint-id: ' + endpointId);
             }
-
             webPhone.utils.extend(data, {
                 extraHeaders: headers
             });
@@ -169,10 +177,6 @@ function registerSIP(checkFlags, transport) {
         });
 }
 
-
-
-
-
 function app() {
     console.log('Sip Registered');
     webPhone.monitor.onUpdate(function() {
@@ -182,21 +186,13 @@ function app() {
     });
 }
 
-
-
-
-
-
 function register(apikey,apisecret,username,password){
-
     var sdk = new RingCentral.SDK({
         appKey: apikey, //localStorage.webPhoneAppKey,
         appSecret: apisecret,//localStorage.webPhoneAppSecret,
         server: RingCentral.SDK.server.sandbox
     });
-
     platform = sdk.platform();
-
     platform
         .login({
             username: username,// localStorage.webPhoneLogin,
@@ -206,5 +202,4 @@ function register(apikey,apisecret,username,password){
             return registerSIP();
         })
         .then(app);
-
 }
